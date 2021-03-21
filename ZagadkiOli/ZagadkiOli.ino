ea79e6c5cec222ae27c662ad600dd745fe06237d
plusz <Konto@Github410>
2021-03-21 15:47:42 +0100

    buzzer added

--- ZagadkiOli/ZagadkiOli.ino
+++ ZagadkiOli/ZagadkiOli.ino
@@ -5,6 +5,7 @@ When success, green LED will shw
 After 5 seconds you can try again 
 */
 #include <LiquidCrystal.h> 
+#include "pitches.h"
 // initialize the library by associating any needed LCD interface pin
 // with the arduino pin number it is connected to
 // piny do podlaczenia ekranu
@@ -19,6 +20,18 @@ int buttonState = 0;         // button state LOW / HIGH
 int ledRed = 10;   // led red pin
 int ledGreen = 8;  // led green pin
 int ledBlue = 13;  // led blue pin
+
+int buzzerPin = 7;
+
+int melody[] = {
+  NOTE_C4, NOTE_G3, NOTE_G3, NOTE_A3, NOTE_G3, 0, NOTE_B3, NOTE_C4
+};
+
+// note durations: 4 = quarter note, 8 = eighth note, etc.:
+int noteDurations[] = {
+  4, 8, 8, 4, 4, 4, 4, 4
+};
+
 
 void setup() {
   Serial.begin(9600);
@@ -40,6 +53,8 @@ void initializeGame() {
   ledHigh(ledRed);    // dioda czerwona zaswiecona
   ledLow(ledGreen);
   ledLow(ledBlue);
+
+  noTone(buzzerPin);
 
   lcd.clear();
   lcd.print(randomNumber);         // display random
@@ -70,7 +85,8 @@ void gra() {
         ledLow(ledRed);
         ledHigh(ledGreen);
         ledLow(ledBlue);  // turn off blue - success state can be modified, to blink etc.
-        
+
+        success();
         delay(5000);  
         initializeGame();  // try one more time
       }
@@ -101,3 +117,30 @@ void gra() {
   void ledLow(int color) {
     digitalWrite(color, LOW);
   }  
+
+
+  void success() {
+    for (int thisNote = 0; thisNote < 8; thisNote++) {
+
+    // to calculate the note duration, take one second divided by the note type.
+
+    //e.g. quarter note = 1000 / 4, eighth note = 1000/8, etc.
+
+    int noteDuration = 1000 / noteDurations[thisNote];
+
+    tone(buzzerPin, melody[thisNote], noteDuration);
+
+    // to distinguish the notes, set a minimum time between them.
+
+    // the note's duration + 30% seems to work well:
+
+    int pauseBetweenNotes = noteDuration * 1.30;
+
+    delay(pauseBetweenNotes);
+
+    // stop the tone playing:
+
+    noTone(buzzerPin);
+
+  }
+  }
